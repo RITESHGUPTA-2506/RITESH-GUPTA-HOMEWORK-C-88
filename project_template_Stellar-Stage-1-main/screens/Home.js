157479b7f0695ddf62e4f375f0ab94342bc929b0
RITESHGUPTA-2506 <112184015+RITESHGUPTA-2506@users.noreply.github.com>
2023-06-02 20:09:33 +0530

    Add files via upload

---
+++ project_template_Stellar-Stage-1-main/screens/Home.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import {
+    Text,
+    View,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    TouchableOpacity
+} from "react-native";
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea}/>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.titleText}>STELLAR APP</Text>
+                    </View>
+                    <TouchableOpacity style={styles.routeCard}>
+                        <Text style = {styles.routeText}>SPACECRAFTS</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.routeCard}>
+                        <Text style = {styles.routeText}>STAR MAP</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.routeCard}>
+                        <Text style = {styles.routeText}>DAILY PICTURES</Text>
+                    </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    titleBar: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "white"
+    },
+    routeCard: {
+        flex: 0.25,
+        marginLeft: 50,
+        marginRight: 50,
+        marginTop: 50,
+        borderRadius: 30,
+        backgroundColor: 'white'
+    },
+    routeText:{
+        fontSize:35,
+        fontWeight:"bold",
+        paddingLeft:50,
+        marginTop:75,
+    },
+    knowMore:{
+        paddingLeft:30,
+        color:'red',
+        fontSize:15,
+    }
+});
